c63ffaf45f50931801cdac3f2ae3addc6d2cc97f
Gonzalo <mgmediaweb@gmail.com>
2022-08-23 13:10:02 -0700

    fix rubocop bug

--- person.rb
+++ person.rb
@@ -7,7 +7,7 @@ class Person < Nameable
   def initialize(
     age,
     name = 'Unknown',
-    parent_permission = true
+    parent_permission: true
   )
     super()
     @id = Random.rand(1..1000)

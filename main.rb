8168e10f7cc2ac0ffd2cd59890f13ca113fb8424
Gonzalo <mgmediaweb@gmail.com>
2022-08-23 13:08:49 -0700

    Âfix the suggestion of reviwer

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ require_relative './teacher'
 require_relative './capitalize_decorator'
 require_relative './trimmer_decorator'
 
-person = Person.new(22, 'maximilianus')
+person = Person.new(354, 'Frankestein')
 person.correct_name
 capitalized_person = CapitalizeDecorator.new(person)
 capitalized_person.correct_name

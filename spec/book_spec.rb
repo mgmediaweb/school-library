458b35367bf00bd0a4e394a267765f176cb0acd0
Gonzalo <mgmediaweb@gmail.com>
2022-08-30 21:44:55 -0700

    fix a rubocop bug

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -8,7 +8,7 @@ describe Book do
 
   describe 'View Book' do
     it 'Show title of book' do
-      expect(new_book.title).to eql 'Da vinci code' 
+      expect(new_book.title).to eql 'Da vinci code'
     end
   end
 end
